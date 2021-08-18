4bdfdea9dfd647d3ca227daef090a41334b81241
Alex Rodrigues <alexrodrigues.ygm@gmail.com>
2021-08-18 18:27:50 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+<section>
+    <cabecalho/>
+    <adicionar-pet/>
+    <rodape/>
+</section>
+</template>
+
+<script>
+import Cabecalho from '@/components/Cabecalho';
+import Rodape from '@/components/Rodape';
+import AdicionarPet from './components/AdicionarPet.vue';
+export default {
+  name: "App",
+  components:{Cabecalho, Rodape, AdicionarPet},
+  data() {
+    return {
+    };
+  },
+};
+</script>
+
+<style>
+
+</style>
